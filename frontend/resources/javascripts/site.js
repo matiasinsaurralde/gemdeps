a74d262592f62b75d4e2cd416a5ec4feb9dacb59
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-08-30 22:16:50 -0400

    adding an extra js

---
+++ frontend/resources/javascripts/site.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+	$('#modal-btn')[0].click();
+	$('#gem_name').focus();
+
+	// render( document.getElementById('viz') );
+
+	$('.pure-button').click( function() {
+					alert( 0 );
+				});
+});
